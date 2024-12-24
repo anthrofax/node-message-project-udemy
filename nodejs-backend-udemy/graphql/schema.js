d67ca45e9fc1b8d72e8c5d3861d5b89de7147ca4
anthrofax <afridhoikhsan@gmail.com>
2024-12-24 09:26:12 +0700

    424. Defining a Mutation Schema

--- nodejs-backend-udemy/graphql/schema.js
+++ nodejs-backend-udemy/graphql/schema.js
@@ -1,18 +1,36 @@
 const { buildSchema } = require("graphql");
 
-const schema = buildSchema(`
+module.exports = buildSchema(`
+    type Post {
+        _id: ID!
+        title: String!
+        content: String!
+        imageUrl: String!
+        creator: User!
+        createdAt: String!
+        updatedAt: String!
+    }
+    
+    type User {
+        _id: ID!
+        name: String!
+        password: String
+        email: String!
+        status: String!
+        posts: [Post!]!
+    }
+    
+    input UserInputData {
+        email: String!
+        password: String!
+        name: String!
+    }
+    
+    type RootMutation {
+        createUser(userInput: UserInputData): User!
+    }
+    
     schema {
-        query: RootQuery
-    }
-
-    type RootQuery {
-        hello: TestData
-    }
-
-    type TestData {
-        text: String!
-        views: Int!
+        mutation: RootMutation
     }
     `);
-
-module.exports = schema;

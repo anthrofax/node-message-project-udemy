7ce3a4f10f3b7aeaa24bff61e7acb2238f7fe49e
anthrofax <afridhoikhsan@gmail.com>
2024-12-25 14:19:00 +0700

    431. Adding a Create Post Mutation

--- nodejs-backend-udemy/graphql/resolver.js
+++ nodejs-backend-udemy/graphql/resolver.js
@@ -1,87 +1,109 @@
 const bcrypt = require("bcryptjs");
 const validator = require("validator");
-const jwt = require('jsonwebtoken')
+const jwt = require("jsonwebtoken");
 
 const User = require("../models/user");
+const Post = require("../models/post");
 
 module.exports = {
-  createUser: async ({ userInput }) => {
-    // Mendapatkan data dari args
-    const { email, name, password } = userInput;
+  createUser: async function ({ userInput }, req) {
+    //   const email = args.userInput.email;
     const errors = [];
-
-    if (!validator.isEmail(email)) {
-      errors.push({
-        message: "Email yang anda inputkan tidak valid.",
-      });
+    if (!validator.isEmail(userInput.email)) {
+      errors.push({ message: "E-Mail is invalid." });
     }
-
     if (
-      validator.isEmpty(password) ||
-      !validator.isLength(password, { min: 5 })
+      validator.isEmpty(userInput.password) ||
+      !validator.isLength(userInput.password, { min: 5 })
     ) {
-      errors.push({
-        message: "Kata sandi harus terdiri dari minimal 5 karakter.",
-      });
+      errors.push({ message: "Password too short!" });
     }
-
     if (errors.length > 0) {
-      const err = new Error(errors[0].message);
-      err.data = errors;
-      err.code = 422;
-
-      throw err;
+      const error = new Error("Invalid input.");
+      error.data = errors;
+      error.code = 422;
+      throw error;
     }
-
-    // Memeriksa apakah pengguna dengan email yang sama sudah ada
-    const existingUser = await User.findOne({ email });
+    const existingUser = await User.findOne({ email: userInput.email });
     if (existingUser) {
-      throw new Error("User exists already!");
+      const error = new Error("User exists already!");
+      throw error;
     }
-
-    // Hashing password
-    const hashedPassword = await bcrypt.hash(password, 12);
-
-    // Membuat pengguna baru
+    const hashedPw = await bcrypt.hash(userInput.password, 12);
     const user = new User({
-      email,
-      name,
-      password: hashedPassword,
+      email: userInput.email,
+      name: userInput.name,
+      password: hashedPw,
     });
-
-    // Menyimpan pengguna ke database
     const createdUser = await user.save();
-
-    // Mengembalikan data pengguna
-    return {
-      ...createdUser._doc,
-      _id: createdUser._id.toString(),
-    };
+    return { ...createdUser._doc, _id: createdUser._id.toString() };
   },
-  login: async ({ email, password }) => {
-    // Cari pengguna berdasarkan email
-    const user = await User.findOne({ email });
+  login: async function ({ email, password }) {
+    const user = await User.findOne({ email: email });
     if (!user) {
-      const error = new Error("User not found!");
+      const error = new Error("User not found.");
       error.code = 401;
       throw error;
     }
-
-    // Periksa password
     const isEqual = await bcrypt.compare(password, user.password);
     if (!isEqual) {
-      const error = new Error("Password is incorrect!");
+      const error = new Error("Password is incorrect.");
       error.code = 401;
       throw error;
     }
-
-    // Buat token JWT
     const token = jwt.sign(
-      { userId: user._id.toString(), email: user.email },
-      "supersecretkey", // Ganti dengan secret key Anda
+      {
+        userId: user._id.toString(),
+        email: user.email,
+      },
+      "somesupersecretsecret",
       { expiresIn: "1h" }
     );
+    return { token: token, userId: user._id.toString() };
+  },
+  createPost: async function ({ postInput }, req) {
+    if (!req.isAuth) {
+      const error = new Error("Not authenticated!");
+      error.code = 401;
+      throw error;
+    }
+    const errors = [];
+    if (
+      validator.isEmpty(postInput.title) ||
+      !validator.isLength(postInput.title, { min: 5 })
+    ) {
+      errors.push({ message: "Title is invalid." });
+    }
+    if (
+      validator.isEmpty(postInput.content) ||
+      !validator.isLength(postInput.content, { min: 5 })
+    ) {
+      errors.push({ message: "Content is invalid." });
+    }
+    if (errors.length > 0) {
+      const error = new Error("Invalid input.");
+      error.data = errors;
+      error.code = 422;
+      throw error;
+    }
+    const user = await User.findById(req.userId);
+    if (!user) {
+      const error = new Error("Invalid user.");
+      error.code = 401;
+      throw error;
+    }
+    const post = new Post({
+      title: postInput.title,
+      content: postInput.content,
+      imageUrl: postInput.imageUrl,
+    });
+    const createdPost = await post.save();
 
-    return { token, userId: user._id.toString() };
+    return {
+      ...createdPost._doc,
+      _id: createdPost._id.toString(),
+      createdAt: createdPost.createdAt.toISOString(),
+      updatedAt: createdPost.updatedAt.toISOString(),
+    };
   },
 };
--- nodejs-backend-udemy/graphql/schema.js
+++ nodejs-backend-udemy/graphql/schema.js
@@ -1,4 +1,4 @@
-const { buildSchema } = require("graphql");
+const { buildSchema } = require('graphql');
 
 module.exports = buildSchema(`
     type Post {
@@ -10,37 +10,44 @@ module.exports = buildSchema(`
         createdAt: String!
         updatedAt: String!
     }
-    
+
     type User {
         _id: ID!
         name: String!
-        password: String
         email: String!
+        password: String
         status: String!
         posts: [Post!]!
     }
 
     type AuthData {
-        userId: String!
         token: String!
+        userId: String!
     }
-    
+
     input UserInputData {
         email: String!
-        password: String!
         name: String!
+        password: String!
     }
-    
-    type RootMutation {
-        createUser(userInput: UserInputData): User!
+
+    input PostInputData {
+        title: String!
+        content: String!
+        imageUrl: String!
     }
 
     type RootQuery {
         login(email: String!, password: String!): AuthData!
     }
-    
-    schema {
-        mutation: RootMutation
-        query: RootQuery
+
+    type RootMutation {
+        createUser(userInput: UserInputData): User!
+        createPost(postInput: PostInputData): Post!
     }
-    `);
+
+    schema {
+        query: RootQuery
+        mutation: RootMutation
+    }
+`);
